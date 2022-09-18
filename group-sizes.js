4fb5360afd7df4d631a3968206ffad7deb5db4d9
Michael-J-Fernandez <michael.fernandez@codeimmersives.com>
2022-09-18 09:04:06 -0400

    Finish grouping program.

--- group-sizes.js
+++ group-sizes.js
@@ -1,24 +1,21 @@
-// THIS IS A WORK IN PROGRESS AND WILL BE FINISHED ON SATURDAY SEPT 17
-
-
 const prompt = require('prompt-sync')({sigint: true});
 
 let classSize = Number(prompt("Enter class size: "))
-// console.log(classSize);
 
+groupsOf2 = 0
+groupsOf3 = 0
 
-if (classSize % 3 === 0) {
+if (classSize < 2) {
+    console.log("You don't have enough students for groups!");
+} else if (classSize % 3 === 0) {
+    groupsOf2 = 0
     groupsOf3 = classSize / 3
+} else if (classSize % 3 === 2) {
+    groupsOf2 = 1
+    groupsOf3 = (classSize - 2) / 3
+} else if (classSize % 3 < 2) {
+    groupsOf2 = 2
+    groupsOf3 = (classSize - 4) / 3
 }
 
-if ()
-
-console.log(`You can make ${groupsOf3} groups of three.`);
-// console.log(`You can make ${groupsOf2} groups of two.`);
-
-// let 
-// let groupsOf2 = 0;
-
-// if (classSize % 3 === 0) {
-//     console.log(`You can make ${classSize / 3} groups of three.`);
-// }
+console.log(`You can make ${groupsOf3} groups of three and ${groupsOf2} groups of two.`);
